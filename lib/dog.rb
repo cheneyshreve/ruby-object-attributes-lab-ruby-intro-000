b912a494f0f7fec68ef18a20de15514b4ebf1221
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-08 21:14:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,15 @@
 class Dog
 
- def name
+ def initialize
   @name = name
+  @breed = breed
  end
 
- def name=(new_name)
-   @name = new_name
+ def name
+   @name = name
  end
 
  def breed
-   @breed = breed
- end
-
- def breed=(new_breed)
    @breed = new_breed
  end
 
